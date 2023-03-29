78c335e7aef621ba1c47e79081461cfedbb2f2bb
Luca Weiss <luca.weiss@fairphone.com>
2023-03-29 10:45:48 +0200

    drivers: irqchip: mpm: remove unused platform support
    Change-Id: I4f03dd9bc10a320dc6095c3084273acca67241ea

--- drivers/irqchip/qcom/mpm.c
+++ drivers/irqchip/qcom/mpm.c
@@ -593,14 +593,6 @@ static const struct of_device_id mpm_gic_chip_data_table[] = {
 		.compatible = "qcom,mpm-gic-msm8953",
 		.data = mpm_msm8953_gic_chip_data,
 	},
-	{
-		.compatible = "qcom,mpm-gic-msm8937",
-		.data = mpm_msm8937_gic_chip_data,
-	},
-	{
-		.compatible = "qcom,mpm-gic-msm8909",
-		.data = mpm_msm8909_gic_chip_data,
-	},
 	{}
 };
 
@@ -610,14 +602,6 @@ static const struct of_device_id mpm_gpio_chip_data_table[] = {
 	{
 		.compatible = "qcom,mpm-gpio-msm8953",
 		.data = mpm_msm8953_gpio_chip_data,
-	},
-	{
-		.compatible = "qcom,mpm-gpio-msm8937",
-		.data = mpm_msm8937_gpio_chip_data,
-	},
-	{
-		.compatible = "qcom,mpm-gpio-msm8909",
-		.data = mpm_msm8909_gpio_chip_data,
 	},
 	{}
 };
